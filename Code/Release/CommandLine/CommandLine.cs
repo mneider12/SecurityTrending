7209355c3c64bb29b73ca79e8f5f43903bd1ee44
Mark Nyder <mneider.virginia@gmail.com>
2019-08-11 22:18:20 -0500

    Create command line menu with create database option

--- Code/Release/CommandLine/CommandLine.cs
+++ Code/Release/CommandLine/CommandLine.cs
@@ -1,20 +1,74 @@
-﻿using Database;
+﻿using System;
+using Database;
 
 namespace CommandLine
 {
     /// <summary>
     /// Entry point for the command line application
     /// </summary>
-    class CommandLine
+    public class CommandLine
     {
         /// <summary>
         /// entry point for the command line application.
         /// creates the application database
         /// </summary>
-        static void Main()
+        public static void Main()
         {
+            // create dependencies here, so they can easily be switched out later
             IDatabase database = new SQLiteDatabase();
-            database.CreateDatabase();
+
+            RunMenu(database);
+        }
+
+        private static void RunMenu(IDatabase database)
+        {
+            string input;
+            Choice choice;
+            do
+            {
+                ShowMenu();
+                input = Console.ReadLine();
+            } while (!ValidateChoice(input, out choice));
+
+            RunChoice(choice, database);
+        }
+
+        private static void ShowMenu()
+        {
+            Console.WriteLine("Select an option");
+            Console.WriteLine("\t[1] Create database");
+        }
+
+        private static bool ValidateChoice(string input, out Choice choice)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                choice = Choice.Quit;
+                return true;
+            }
+
+            if (Enum.TryParse(input, out choice))
+            {
+                return Enum.IsDefined(typeof(Choice), choice);
+            }
+
+            return false;
+        }
+
+        private static void RunChoice(Choice choice, IDatabase database)
+        {
+            switch (choice)
+            {
+                case Choice.Create:
+                    database.CreateDatabase();
+                    break;
+            }
+        }
+
+        private enum Choice
+        {
+            Create = 1,
+            Quit = 999,
         }
     }
 }

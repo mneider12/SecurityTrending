c5fde3a643178a106bb6875c2c0b50aa08e1dd1f
Mark Nyder <mneider.virginia@gmail.com>
2019-08-11 21:52:00 -0500

    Add Classes and Transactions tables

--- Code/Release/SQLiteDatabase/SQLiteDatabase.cs
+++ Code/Release/SQLiteDatabase/SQLiteDatabase.cs
@@ -20,6 +20,14 @@ namespace Database
                 {
                     command.ExecuteNonQuery();
                 }
+                using (SQLiteCommand command = new SQLiteCommand(CREATE_CLASSES_SQL, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+                using (SQLiteCommand command = new SQLiteCommand(CREATE_TRANSACTIONS_SQL, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
                 connection.Close();
             }
         }
@@ -31,5 +39,26 @@ namespace Database
                                                     "\"Name\" TEXT NOT NULL," +
                                                     "PRIMARY KEY(\"ActionID\")" +
                                                     ");";
+        /// <summary>
+        /// SQL command to create the Classes table
+        /// </summary>
+        private const string CREATE_CLASSES_SQL = "CREATE TABLE \"Classes\" (" +
+                                                    "\"ClassID\" INTEGER," +
+                                                    "\"Name\" TEXT NOT NULL," +
+                                                    "PRIMARY KEY(\"ClassID\")" +
+                                                    ");";
+        /// <summary>
+        /// SQL command to create the transactions table
+        /// </summary>
+        private const string CREATE_TRANSACTIONS_SQL = "CREATE TABLE \"Transactions\" (" +
+	                                                    "\"TransactionID\"	INTEGER," +
+	                                                    "\"Date\"	TEXT NOT NULL," +
+	                                                    "\"ActionID\"	INTEGER NOT NULL," +
+	                                                    "\"ClassID\"	INTEGER NOT NULL," +
+	                                                    "\"Ticker\"	TEXT," +
+	                                                    "\"Amount\"	NUMERIC NOT NULL," +
+	                                                    "FOREIGN KEY(\"ActionID\") REFERENCES \"Actions\"(\"ActionID\")," +
+	                                                    "PRIMARY KEY(\"TransactionID\")" +
+                                                        ");";
     }
 }

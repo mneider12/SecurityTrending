b8751732b86c174109c3764184e304ccece6f66d
Mark Nyder <mneider.virginia@gmail.com>
2019-08-13 21:35:10 -0500

    Start command line implementation for transaction entry

---
+++ Code/Release/Core/Transaction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static Core.TransactionEnums;
+
+namespace Core
+{
+    public class Transaction
+    {
+        /// <summary>
+        /// Unique transaction ID
+        /// </summary>
+        public int TransactionID { get; set; }
+
+        /// <summary>
+        /// date transaction occured
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// interaction of transaction with the cash account
+        /// </summary>
+        public TransactionAction Action { get; set; }
+
+        /// <summary>
+        /// additional type information about the transaction
+        /// </summary>
+        public TransactionClass Class { get; set; }
+
+        /// <summary>
+        /// ticker associated with the transaction
+        /// </summary>
+        public string Ticker { get; set; }
+
+        /// <summary>
+        /// amount of the transaction
+        /// </summary>
+        public Decimal Amount { get; set; }
+    }
+}
---
+++ Code/Release/Core/TransactionEnums.cs
@@ -0,0 +1,55 @@
+﻿namespace Core
+{
+    public class TransactionEnums
+    {
+        /// <summary>
+        /// actions describe the transaction interaction with the cash account.
+        /// </summary>
+        public enum TransactionAction
+        {
+            /// <summary>
+            /// buy results in a purchased asset and spent cash
+            /// </summary>
+            buy = 1,
+            /// <summary>
+            /// sell results in a sold asset and recieved cash
+            /// </summary>
+            sell = 2,
+            /// <summary>
+            /// deposit from another account
+            /// </summary>
+            deposit = 3,
+            /// <summary>
+            /// withdrawal to another account
+            /// </summary>
+            withdrawal = 4,
+            /// <summary>
+            /// dividend received
+            /// </summary>
+            dividend = 5,
+            /// <summary>
+            /// interest received
+            /// </summary>
+            interest = 6,
+        }
+
+        /// <summary>
+        /// classes describe additional type information about a transaction
+        /// </summary>
+        public enum TransactionClass
+        {
+            /// <summary>
+            /// both sides of a transaction are cash (i.e. a transfer)
+            /// </summary>
+            cash = 1,
+            /// <summary>
+            /// transaction is related to a stock
+            /// </summary>
+            stock = 2,
+            /// <summary>
+            /// transaction is related to a bond
+            /// </summary>
+            bond = 3,
+        }
+    }
+}

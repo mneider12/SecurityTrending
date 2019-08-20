ea2861324cd70e17e2ef9e80c2f8f4440c3a7a8c
Mark Nyder <mneider.virginia@gmail.com>
2019-08-20 18:57:41 -0500

    Add create database test, only test for table names

---
+++ Code/Test/DatabaseTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DatabaseTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DatabaseTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("38c8156c-99c9-4b09-be4e-d6030e2bc56f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Code/Test/DatabaseTests/SQLiteDatabaseTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using Database;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DatabaseTests
+{
+    /// <summary>
+    /// Unit tests for SQLiteDatabase.cs
+    /// </summary>
+    [TestClass]
+    public class SQLiteDatabaseTest
+    {
+        /// <summary>
+        /// test the CreateDatabase method
+        /// </summary>
+        [TestMethod]
+        public void CreateDatabaseTest()
+        {
+            SQLiteDatabase database = new SQLiteDatabase();
+            database.CreateDatabase();
+            using (SQLiteConnection connection = new SQLiteConnection("Data Source=database.sqlite"))
+            {
+                connection.Open();
+                using (SQLiteCommand command = new SQLiteCommand("select name from sqlite_master order by name;", connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        CheckNextTableName(reader, "Actions");
+                        CheckNextTableName(reader, "Classes");
+                        CheckNextTableName(reader, "Transactions");
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Check that the next table name in a reader matches the expected table name
+        /// </summary>
+        /// <param name="reader">reader with table name in rows</param>
+        /// <param name="expectedTableName">expected table name</param>
+        private void CheckNextTableName(SQLiteDataReader reader, string expectedTableName)
+        {
+            reader.Read();
+            Assert.AreEqual(expectedTableName, reader["name"]);
+        }
+    }
+}

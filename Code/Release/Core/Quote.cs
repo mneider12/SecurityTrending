0833fa810ea99df2c12faa0e7d342a9aa852c108
Mark Nyder <mneider.virginia@gmail.com>
2019-08-26 20:59:14 -0500

    create quote data model

---
+++ Code/Release/Core/Quote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    /// <summary>
+    /// historical data for a security
+    /// </summary>
+    public class Quote
+    {
+        /// <summary>
+        /// closing price
+        /// </summary>
+        public decimal Close { get; set; }
+        /// <summary>
+        /// date of quote
+        /// </summary>
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// security ticker
+        /// </summary>
+        public string Ticker { get; set; }
+    }
+}

8be182aaecdcf5ed703db58cc450bf6d4847f524
Mark Nyder <mneider.virginia@gmail.com>
2019-10-20 12:10:42 -0500

    Add position model

---
+++ Code/Release/Core/Position.cs
@@ -0,0 +1,23 @@
+﻿using static Core.TransactionEnums;
+
+namespace Model
+{
+    /// <summary>
+    /// represents an interest in a single security
+    /// </summary>
+    public class Position
+    {
+        /// <summary>
+        /// Symbol representing the position
+        /// </summary>
+        public string Symbol { get; set; }
+        /// <summary>
+        /// Number of shares of the security
+        /// </summary>
+        public double Shares { get; set; }
+        /// <summary>
+        /// class of the position
+        /// </summary>
+        public TransactionClass Class { get; set; }
+    }
+}

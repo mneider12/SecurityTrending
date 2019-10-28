054c5eb2f1ba66431db35cdb9bbb9dbae42b3997
Mark Nyder <mneider.virginia@gmail.com>
2019-10-27 21:21:03 -0500

    add equals and hash code

--- Code/Release/Core/Position.cs
+++ Code/Release/Core/Position.cs
@@ -19,5 +19,35 @@ namespace Model
         /// class of the position
         /// </summary>
         public TransactionClass Class { get; set; }
+        /// <summary>
+        /// compare two positions for equality
+        /// </summary>
+        /// <param name="obj">position to compare</param>
+        /// <returns>whether two positions are equal</returns>
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Position);
+        }
+        /// <summary>
+        /// get a hash of the properties in the position
+        /// </summary>
+        /// <returns>hash code</returns>
+        public override int GetHashCode()
+        {
+            return Symbol.GetHashCode() + Quantity.GetHashCode() + Class.GetHashCode();
+        }
+        #region private helper methods
+
+        /// <summary>
+        /// check for equality of two positions
+        /// </summary>
+        /// <param name="otherPosition">other position</param>
+        /// <returns>whether two positions are equal</returns>
+        private bool Equals(Position otherPosition)
+        {
+            return otherPosition != null && Symbol == otherPosition.Symbol && Quantity == otherPosition.Quantity && Class == otherPosition.Class;
+        }
+
+        #endregion
     }
 }

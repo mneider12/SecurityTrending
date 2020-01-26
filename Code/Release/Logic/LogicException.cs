d675df142ace9debfce0013a89a6d4120e30692f
Mark Nyder <mneider.virginia@gmail.com>
2020-01-26 15:39:03 -0600

    add required constructors

--- Code/Release/Logic/LogicException.cs
+++ Code/Release/Logic/LogicException.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace Logic
 {
-    interface Interface1
+    /// <summary>
+    /// attempted operation violates a business logic rule
+    /// </summary>
+    public class LogicException : Exception
     {
+        /// <summary>
+        /// default exception constructor
+        /// </summary>
+        public LogicException() : base() { }
+        /// <summary>
+        /// pass a message with the exception
+        /// </summary>
+        /// <param name="message">error message</param>
+        public LogicException(string message) : base(message) { }
+        /// <summary>
+        /// exception caused by another exception
+        /// </summary>
+        /// <param name="message">error message</param>
+        /// <param name="innerException">inner exception</param>
+        public LogicException(string message, Exception innerException) : base(message, innerException) { }
     }
 }

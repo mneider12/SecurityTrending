d6773443d71540e478e0fd3abdd5a098586c5179
Mark Nyder <mneider.virginia@gmail.com>
2020-02-02 13:31:38 -0600

    update files, add position test

---
+++ Code/Solutions/ReportsTests/Metricstest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Database;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using Reports;
+
+namespace ReportsTests
+{
+    /// <summary>
+    /// test the Metrics Class
+    /// </summary>
+    [TestClass]
+    public class MetricsTest
+    {
+        /// <summary>
+        /// test the zero state for position value
+        /// </summary>
+        [TestMethod]
+        public void PositionValueZero()
+        {
+            IDatabase database = new SQLiteDatabase();
+            database.CreateDatabase();
+
+            Position position = new Position()
+            {
+                Symbol = "test",
+                Quantity = 0,
+                Class = Core.TransactionEnums.TransactionClass.stock,
+            };
+
+            decimal expectedPositionValue = 0m;
+            decimal actualPositionValue = Metrics.PositionValue(database, position);
+
+            Assert.AreEqual(expectedPositionValue, actualPositionValue);
+        }
+    }
+}
---
+++ Code/Solutions/ReportsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReportsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReportsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d62ab99a-27e2-4d80-a31b-3b8d026e20a8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

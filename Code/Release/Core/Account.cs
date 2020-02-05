b7f688eb4497a53299dede0b2eec8aefeb4edbc7
Mark Nyder <mneider.virginia@gmail.com>
2020-02-04 20:57:23 -0600

    Create Account model

---
+++ Code/Release/Core/Account.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Model
+{
+    public class Account
+    {
+        public Account()
+        {
+            Positions = new Dictionary<string, Position>();
+        }
+
+        public Dictionary<string, Position> Positions
+        {
+            get;
+        }
+    }
+}

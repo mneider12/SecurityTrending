6c0cb2fcac13bf01f8ef6134b8737c4bee6bf1df
Mark Nyder <mneider.virginia@gmail.com>
2020-02-15 16:09:17 -0600

    test cash transactions loader

--- Code/Test/DataEntryTests/CSVTransactionLoaderTest.cs
+++ Code/Test/DataEntryTests/CSVTransactionLoaderTest.cs
@@ -18,9 +18,67 @@ namespace DataEntryTests
             Account account = new Account();
             ICashTransactionLoader loader = new CSVTransactionLoader();
 
-            List<Transaction> transactions = loader.LoadTransactions(account, fileInfo);
+            List<Transaction> expectedTransactions = new List<Transaction>()
+            {
+                new Transaction()
+                {
+                    Account = account,
+                    Action = TransactionEnums.TransactionAction.deposit,
+                    Amount = 10000,
+                    Class = TransactionEnums.TransactionClass.cash,
+                    Date = new DateTime(2000, 1, 1),
+                    Quantity = 0m,
+                    Symbol = "",
+                    TransactionID = 0,
+                },
 
+                new Transaction()
+                {
+                    Account = account,
+                    Action = TransactionEnums.TransactionAction.deposit,
+                    Amount = 5000,
+                    Class = TransactionEnums.TransactionClass.cash,
+                    Date = new DateTime(2000, 2, 1),
+                    Quantity = 0m,
+                    Symbol = "",
+                    TransactionID = 0,
+                },
 
+                new Transaction()
+                {
+                    Account = account,
+                    Action = TransactionEnums.TransactionAction.withdrawal,
+                    Amount = -7000,
+                    Class = TransactionEnums.TransactionClass.cash,
+                    Date = new DateTime(2000, 3, 1),
+                    Quantity = 0m,
+                    Symbol = "",
+                    TransactionID = 0,
+                }
+            };
+
+            List<Transaction> actualTransactions = loader.LoadTransactions(account, fileInfo);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.IsTrue(AreTransactionsEquivalent(expectedTransactions[i], actualTransactions[i]));
+            }
+            
+        }
+
+        private bool AreTransactionsEquivalent(Transaction transaction1, Transaction transaction2)
+        {
+            if (transaction1.Amount == transaction2.Amount && transaction1.Account == transaction1.Account &&
+                transaction1.Action == transaction2.Action && transaction1.Class == transaction2.Class &&
+                transaction1.Date == transaction2.Date && transaction1.Quantity == transaction2.Quantity &&
+                transaction1.Symbol == transaction2.Symbol && transaction1.TransactionID == transaction2.TransactionID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
